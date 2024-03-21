7c85ebe9472e7004995d900cd59610a9bcfcf3d1
taewoongjung <taewj@howser.co.kr>
2024-03-22 01:15:14 +0900

    로그인, 회원가입 화면 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import App from './layouts/App/index';
 
-render(
+// @ts-ignore
+createRoot(document.getElementById('app')).render(
     <BrowserRouter>
         <App />
-    </BrowserRouter>,
-    document.querySelector('#app'),
+    </BrowserRouter>
 );
