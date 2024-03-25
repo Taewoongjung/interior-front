812c40f87d19dbbe69e01519c2be3a63d8506114
taewoongjung <taewj@howser.co.kr>
2024-03-25 12:30:03 +0900

    회원가입 후 모달 추가
    - "회원가입이 완료 되었습니다."

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,35 @@
+import React, {useState} from "react"
+import './style.scss';
+
+const customStyles = {
+    overlay: {
+        backgroundColor: "rgb(0, 0, 0, 0.5)",
+    },
+    content: {
+        width: "300px",
+        height: "400px",
+        margin: "auto",
+        borderRadius: "4px",
+        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+        padding: "20px",
+    },
+}
+
+const Modal = (props: { children?: any; open?: any; close?: any; header?: string; }) => {
+    // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
+    const { open, close, header } = props
+
+    if (!open) return null;
+
+    return (
+        <>
+            <div className="modal-overlay" onClick={close} />
+            <div className="modal-wrapper">
+                <div className="modal-content"> {header} </div>
+                <button className="modal-close-button" onClick={close}>닫기</button>
+            </div>
+        </>
+    )
+};
+
+export default Modal;
--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import './styles.scss';
 import {useForm, useFormState} from "react-hook-form";
 import {IFormValues} from "../../definitions/IFormValues";
-import useInput from "../../hooks/useInput";
+import Modal from "../../components/Modal";
 
 const LogIn = () => {
     const [isLogIn, setIsLogIn] = useState(false);
@@ -28,12 +28,19 @@ const LogIn = () => {
         const tel = "01011231237";
         const role = "ADMIN";
 
-        axios.post("http://interiorjung.shop:707/api/signup", {
+        axios.post("http://localhost:7070/api/signup", {
             name, email, password, tel, role
             }, {
                 withCredentials: true // CORS 처리 옵션
             }
-        ).catch((error) => {
+        ).then((response) => {
+            if (response.data.isSuccess === true) {
+                openModal();
+                setModalHeader("회원가입이 완료 되었습니다.")
+                setIsLogIn(false);
+            }
+    })
+        .catch((error) => {
             console.dir(error);
         });
     };
@@ -101,88 +108,100 @@ const LogIn = () => {
         delayError: undefined
     })
 
+
+    const [isModalOpen, setIsModalOpen] = useState(false); // 모달의 열림 상태를 관리하는 상태 변수
+    const [modalHeader, setModalHeader] = useState(""); // 모달의 열림 상태를 관리하는 상태 변수
+
+    // 모달을 열기 위한 함수
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    // 모달을 닫기 위한 함수
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
-        <section className={isLogIn ? 'active' : ''}>
-            <div className="left">
-                <img src="/login/interior-jung-title-login.png" alt="로그인 이미지" width="700" height="400" />
-                <div className="sign-up">
-                    <h1>회원가입</h1>
-                    <form onSubmit={handleSubmit(onSubmitSignUp)}>
-                        <input
-                            id="name" placeholder="유저 이름"
-                            {...register("name", {
-                                required: "이름은 필수 응답 항목입니다.",
-                                pattern: {
-                                    value: /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣]{2,7}$/,
-                                    message: "이름 형식이 맞지않습니다."
-                                },
-                            })}
-                        />
-                        {errors.name && <div>{errors.name?.message}</div>}
+        <>
+            <section className={isLogIn ? 'active' : ''}>
+                <div className="left">
+                    <img src="/login/interior-jung-title-login.png" alt="로그인 이미지" width="700" height="400"/>
+                    <div className="sign-up">
+                        <h1>회원가입</h1>
+                        <form onSubmit={handleSubmit(onSubmitSignUp)}>
+                            <input
+                                id="name" placeholder="유저 이름"
+                                {...register("name", {
+                                    required: "이름은 필수 응답 항목입니다.",
+                                    pattern: {
+                                        value: /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣]{2,7}$/,
+                                        message: "이름 형식이 맞지않습니다."
+                                    },
+                                })} />
+                            {errors.name && <div>{errors.name?.message}</div>}
 
-                        <input
-                            placeholder="이메일"
-                            {...register("email", {
-                                required:"이메일은 필수 응답 항목입니다.",
-                                pattern:{
-                                    value:/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i,
-                                    message:"이메일 형식이 아닙니다."
-                                }
-                            })}
-                        />
-                        {errors.email && <div>{errors.email?.message}</div>}
+                            <input
+                                placeholder="이메일"
+                                {...register("email", {
+                                    required: "이메일은 필수 응답 항목입니다.",
+                                    pattern: {
+                                        value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i,
+                                        message: "이메일 형식이 아닙니다."
+                                    }
+                                })} />
+                            {errors.email && <div>{errors.email?.message}</div>}
 
-                        <input
-                            placeholder="비밀번호"
-                            {...register("password", {
-                                required: '비밀번호는 필수 입력입니다.',
-                                pattern: {
-                                    value: /^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$/,
-                                    message:
-                                        '영문, 숫자를 포함한 6자 이상의 비밀번호를 입력해주세요.',
-                                },
-                            })}
-                        />
-                        {errors.password && <div>{errors.password?.message}</div>}
+                            <input
+                                placeholder="비밀번호"
+                                {...register("password", {
+                                    required: '비밀번호는 필수 입력입니다.',
+                                    pattern: {
+                                        value: /^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$/,
+                                        message: '영문, 숫자를 포함한 6자 이상의 비밀번호를 입력해주세요.',
+                                    },
+                                })} />
+                            {errors.password && <div>{errors.password?.message}</div>}
 
-                        <input
-                            placeholder="비밀번호 재확인"
-                            {...register("reCheckPassword", {
-                                required: '비밀번호는 필수 입력입니다.',
-                                pattern: {
-                                    value: /^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$/,
-                                    message:
-                                        '영문, 숫자를 포함한 6자 이상의 비밀번호를 입력해주세요.',
-                                },
-                            })}
-                        />
-                        {errors.reCheckPassword && <div>{errors.reCheckPassword?.message}</div>}
-                        <input type="submit" value="회원가입하기" />
-                    </form>
-                    <p>
-                        이미 계정이 있으신가요?
-                        <a href="#!" id="sign-in" onClick={toggleForm}>로그인 하러 가기.</a>
-                    </p>
+                            <input
+                                placeholder="비밀번호 재확인"
+                                {...register("reCheckPassword", {
+                                    required: '비밀번호는 필수 입력입니다.',
+                                    pattern: {
+                                        value: /^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$/,
+                                        message: '영문, 숫자를 포함한 6자 이상의 비밀번호를 입력해주세요.',
+                                    },
+                                })} />
+                            {errors.reCheckPassword && <div>{errors.reCheckPassword?.message}</div>}
+                            <input type="submit" value="회원가입하기"/>
+                        </form>
+                        <p>
+                            이미 계정이 있으신가요?
+                            <a href="#!" id="sign-in" onClick={toggleForm}>로그인 하러 가기.</a>
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div className="right">
-                <img src="/signup/interior-jung-title-signup.jpg" alt="회원가입 이미지" width="700" height="400" />
-                <div className="sign-in">
-                    <h1>로그인</h1>
-                    <form onSubmit={onSubmit}>
-                        <input type="email" onChange={onChangeEmail} placeholder="이메일" />
-                        {emailError && <div>{emailError}</div>}
+                <div className="right">
+                    <img src="/signup/interior-jung-title-signup.jpg" alt="회원가입 이미지" width="700" height="400"/>
+                    <div className="sign-in">
+                        <h1>로그인</h1>
+                        <form onSubmit={onSubmit}>
+                            <input type="email" onChange={onChangeEmail} placeholder="이메일"/>
+                            {emailError && <div>{emailError}</div>}
 
-                        <input type="password" onChange={onChangePassword} placeholder="패스워드" />
-                        {passwordError && <div>{passwordError}</div>}
-                        <input type="submit" value="로그인하기" />
-                    </form>
-                    <p>
-                        아직 계정이 없으신가요? <a href="#!" onClick={toggleForm}>회원가입 하기.</a>
-                    </p>
+                            <input type="password" onChange={onChangePassword} placeholder="패스워드"/>
+                            {passwordError && <div>{passwordError}</div>}
+                            <input type="submit" value="로그인하기"/>
+                        </form>
+                        <p>
+                            아직 계정이 없으신가요? <a href="#!" onClick={toggleForm}>회원가입 하기.</a>
+                        </p>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+
+            <Modal open={isModalOpen} close={closeModal} header={modalHeader}/>
+        </>
     );
 };
 
