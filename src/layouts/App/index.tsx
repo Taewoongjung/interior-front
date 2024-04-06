ad45e0ca76e48fb58da3fd283bb222ef5b5706d0
taewoongjung <aipooh8882@naver.com>
2024-04-06 22:21:13 +0900

    Ht 페이지 추가

--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -4,6 +4,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 const Auth = loadable(() => import('../../pages/Auth/index'));
 const Main = loadable(() => import('../../pages/Main/index'))
+const Ht = loadable(() => import('../../pages/Test/index'));
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <Redirect exact path="/" to="/auth" />
           <Route path="/auth" component={Auth} />
           <Route path="/main" component={Main} />
+          <Route path="/ht" component={Ht} />
       </Switch>
   )
 }
---
+++ src/pages/Test/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Test = () => {
+
+    return (
+        <>
+            <img src="assets/imgs/ht.png" alt=""/>
+        </>
+    );
+}
+
+export default Test;
