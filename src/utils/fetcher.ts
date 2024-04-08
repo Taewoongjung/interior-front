f52f4edaf8b390c3ff589a2019304c211870c3d6
taewoongjung <taewj@howser.co.kr>
2024-04-09 00:22:47 +0900

    nginx location 수정

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const fetcher = async (url: string) => await axios.get(url, {
     headers: {
-        Authorization: localStorage.getItem("interiorjung-token")
+        Authorization: localStorage.getItem("interiorjung-token"),
+        withCredentials: true
     },
-    withCredentials: true,
 }).then((response) => {
 
     console.log("token = ", localStorage.getItem("interiorjung-token"));
