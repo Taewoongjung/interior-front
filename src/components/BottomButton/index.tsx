f88982b04402bd606d44bc5ba6b0cee89a649cd5
taewoongjung <taewj@howser.co.kr>
2024-04-12 13:57:12 +0900

    MainNav 사업등록창 추가

--- src/components/BottomButton/index.tsx
+++ src/components/BottomButton/index.tsx
@@ -7,7 +7,6 @@ const BottomButton = () => {
             {/*Social Icons*/}
             <ul className="icons">
                 <li><a href="https://github.com/Taewoongjung" className="icon brands fa-github"><span className="label">Github</span></a></li>
-                <li><a href="dndxowjd@gmail.com" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
             </ul>
         </>
     )
--- src/components/Nav/Main/index.tsx
+++ src/components/Nav/Main/index.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
 import useSWR from "swr";
 import fetcher from "../../../utils/fetcher";
 import {useHistory, useParams} from 'react-router-dom';
+import axios from "axios";
+import "./styles.css";
 
 const MainNav = (() => {
 
@@ -27,13 +29,55 @@ const MainNav = (() => {
         });
     };
 
+    const [businessName, setBusinessName] = useState('');
+
+    const onChangeBusinessName = (e: { target: { value: string; }; }) => {
+        const value = e.target.value;
+        setBusinessName(value);
+    };
+
+    const onSubmitCreateBusiness = useCallback(async (e: { preventDefault: () => void; }) => {
+            e.preventDefault();
+
+            await axios
+                .post("http://api-interiorjung.shop:7077/api/businesses", {
+                // .post("http://localhost:7070/api/businesses", {
+                        businessName
+                    }, {
+                        withCredentials: true, // CORS 처리 옵션
+                        headers: {
+                            Authorization: localStorage.getItem("interiorjung-token")
+                        }
+                    }
+                ).then((response) => {
+                        if (response.data === true) {
+                            mutate();
+                        }
+                })
+                .catch((error) => {
+                    console.dir("error = ", error);
+                });
+        },
+        [businessName]
+    );
+
     return(
         <>
-            <ul>
+            <ul className="main-nav-input-text-ul">
                 {/*<li><a href="#top" id="top-link"><span className="icon solid fa-home">Intro</span></a></li>*/}
                 {/*<li><a href="#portfolio" id="portfolio-link"><span className="icon solid fa-th">Portfolio</span></a></li>*/}
                 {/*<li><a href="#about" id="about-link"><span className="icon solid fa-user">About Me</span></a></li>*/}
                 {/*<li><a href="#contact" id="contact-link"><span className="icon solid fa-envelope">Contact</span></a></li>*/}
+
+                <form onSubmit={onSubmitCreateBusiness}>
+                    <div className="input-block">
+                        <input id="input-text" value={businessName} placeholder="추가 할 사업" onChange={onChangeBusinessName} required={true}/>
+                        <span className="main-nav-input-text-span"></span>
+
+                        <input className="main-nav-input-text-submit" type="submit" value="사업등록"/>
+                    </div>
+                </form>
+
                 {businesses?.map((business: { id: string; name: React.ReactNode; }) => (
                     <li key={business.id}>
                         <a onClick={() => handleButtonClick(business.id)}><span className="icon solid fa-th">{business.name}</span></a>
