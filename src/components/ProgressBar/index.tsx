cbfbb1d36a14cf50745c3350e63dfc1f5d1e1651
taewoongjung <taewj@howser.co.kr>
2024-06-07 14:18:36 +0900

    progressBar 로직 수정

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -17,26 +17,47 @@ const ProgressBar = (props:{ taskId:string; setProgressBarModalOpen:any; }) => {
     console.log("progress", taskId);
 
     useEffect(() => {
-
         if (!taskId) return;
 
         const eventSource = new EventSource(
             `http://api-interiorjung.shop:7077/api/excels/tasks/${taskId}`,
-            // `http://localhost:7070/api/excels/tasks/${taskId}`,
-            {
-            withCredentials: true, // CORS 처리 옵션
+            // `http://localhost:7070/api/excels/tasks/${taskId}`
+        );
+
+        eventSource.onopen = () => {
+            console.log("Connection to EventSource established.");
+        };
+
+        eventSource.addEventListener(taskId, (e:any) => {
+            console.log("11 = ",e.data);
+            if (e && e.data) {
+                try {
+                    const streamData = JSON.parse(e.data);
+
+                    if (streamData && streamData.completeCount != null && streamData.totalCount != null) {
+                        const completeCount = Number(streamData.completeCount);
+                        const totalCount = Number(streamData.totalCount);
+                        const newPercentage = Math.round((completeCount / totalCount) * 100);
+
+                        setPercentage(newPercentage);
+                    } else {
+                        console.error("Incomplete data received: ", streamData);
+                    }
+                } catch (error) {
+                    console.error("Error parsing data: ", error);
+                }
+            }
         });
 
-        eventSource.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            if (event !== null) {
-                setPercentage(Math.round((data.completeCount / data.totalCount) * 100))
-            }
+        eventSource.onerror = (err) => {
+            console.error("EventSource error: ", err);
+            eventSource.close();
         };
 
         return () => {
             setProgressBarModalOpen(false);
             eventSource.close(); // 컴포넌트 언마운트 시 EventSource를 닫습니다.
+            console.log("EventSource closed.");
         };
     }, [taskId]);
 
