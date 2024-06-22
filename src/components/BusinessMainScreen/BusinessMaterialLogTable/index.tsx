da969f81476663a1d8b201ec7a77a73984164bbf
taewoongjung <taewj@howser.co.kr>
2024-06-22 13:36:08 +0900

    [UX] 재료 변경 로그 각 필드 별로 태그 색상 변경

--- src/components/BusinessMainScreen/BusinessMaterialLogTable/index.tsx
+++ src/components/BusinessMainScreen/BusinessMaterialLogTable/index.tsx
@@ -31,12 +31,40 @@ const BusinessMaterialLogTable = (props: { logData: DataType[] }) => {
     ];
 
     const getColor = (type: any) => {
-        if (type === '삭제') {
-            return '#f50';
+        if (type === '재료생성') {
+            return '#F44336';
         }
 
-        if (type === '생성') {
-            return '#2db7f5';
+        if (type === '재료삭제') {
+            return '#4CAF50';
+        }
+
+        if (type === '재료명') {
+            return '#2196F3';
+        }
+
+        if (type === '재료 카테고리') {
+            return '#9C27B0';
+        }
+
+        if (type === '재료 수량') {
+            return '#FFC107';
+        }
+
+        if (type === '재료 수량 단위') {
+            return '#FF9800';
+        }
+
+        if (type === '재료 메모') {
+            return '#795548';
+        }
+
+        if (type === '재료의 단가') {
+            return '#03A9F4';
+        }
+
+        if (type === '재료 노무비 단가') {
+            return '#8BC34A';
         }
 
         return '#87d068';
